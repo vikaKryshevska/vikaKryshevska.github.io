66526da860302c509678e130b79ac89c2c11a300
Versky <113776320+vikaKryshevska@users.noreply.github.com>
2023-05-28 16:14:58 +0300

    list of products done

--- script.js
+++ script.js
@@ -25,72 +25,6 @@ menuItems.forEach(
 
 
 
-// Отримуємо контейнер, в який будемо додавати продукти
-
-
-let jsonData = [
-  {
-     name: "Saurabh",
-     age: "20",
-     city: "Prayagraj"
-  },
-  {
-     name: "Vipin",
-     age: 23,
-     city: "Lucknow",
-  },
-  {
-     name: "Saksham",
-     age: 21,
-     city: "Noida"
-  }
-];
-/* 
-// Get the container element where the table will be inserted
-let container = document.getElementById("products-list");
-
-// Create the table element
-let table = document.createElement("table");
-
-// Get the keys (column names) of the first object in the JSON data
-let cols = Object.keys(jsonData[0]);
-
-// Create the header element
-let thead = document.createElement("thead");
-let tr = document.createElement("tr");
-
-// Loop through the column names and create header cells
-cols.forEach((item) => {
-  let th = document.createElement("th");
-  th.innerText = item; // Set the column name as the text of the header cell
-  tr.appendChild(th); // Append the header cell to the header row
-});
-thead.appendChild(tr); // Append the header row to the header
-table.append(tr) // Append the header to the table
-
-// Loop through the JSON data and create table rows
-jsonData.forEach((item) => {
-  let tr = document.createElement("tr");
-  
-  // Get the values of the current object in the JSON data
-  let vals = Object.values(item);
-  
-  // Loop through the values and create table cells
-  vals.forEach((elem) => {
-     let td = document.createElement("td");
-     td.innerText = elem; // Set the value as the text of the table cell
-     tr.appendChild(td); // Append the table cell to the table row
-  });
-  table.appendChild(tr); // Append the table row to the table
-});
-container.appendChild(table) // Append the table to the container element
-
-
- */
-
-
-
-
 
 
 fetch('products.json')
@@ -144,7 +78,7 @@ function appendData(data) {
 
   const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
-const images = document.querySelector('.carousel').children;
+const images = document.querySelector('.slider').children;
 const totalImages = images.length;
 let index = 0;
 
